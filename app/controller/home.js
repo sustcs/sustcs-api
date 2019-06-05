ccbabc2abe66f182d3c0e1f14471b92b411ce7e6
makergyt <me@makergyt.com>
2019-06-05 21:57:35 +0800

    webhook

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, hi';
+    ctx.body = 'hi, hi,hi';
   }
   async search() {
     const { ctx } = this;
